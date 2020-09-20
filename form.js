f45e015cc742c34f3eb856cabd7b55115fa6abf0
Skandak99 <69813418+Skandak99@users.noreply.github.com>
2020-09-20 16:57:56 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,20 @@
+
+function buy() {
+    document.getElementById("button").style.display="none"
+    document.getElementById("thanks").style.display="inherit"
+}
+
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+  }
+  
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
